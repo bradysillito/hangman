c3683e8651fba1912f07b57c9910a7abfd7bb111
Brady Sillito <sillito121@gmail.com>
2022-10-29 12:09:20 -0600

    finished Hangman class with functionality to play one game

---
+++ src/HangmanGame.java
@@ -0,0 +1,70 @@
+public class HangmanGame {
+    private String wordToGuess;
+    private int totalGuess;
+
+    private String userGuess;
+
+    public HangmanGame(String wordToGuess) {
+        this.wordToGuess = wordToGuess;
+        this.totalGuess = 0;
+    }
+
+    public void startGame() {
+        System.out.println("Let's play!");
+        System.out.println("The word to guess has " + wordToGuess.length() + " letters!");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            sb.append("_");
+        }
+
+        this.userGuess = sb.toString();
+        System.out.println(sb);
+    }
+
+    public String getUserGuess() {
+        return userGuess;
+    }
+
+    public String checkUserGuess(char charAt) {
+        //check if the char is a letter
+        if(!Character.isLetter(charAt)){
+            System.out.println("You can only guess letters");
+            return userGuess;
+        }
+
+        //update the amount of guesses made
+        this.totalGuess ++;
+
+        boolean correctGuess = false;
+        //check if the wordToGuess contains the user's guessed letter
+        if(wordToGuess.contains(Character.toString(charAt))){
+            //create a string builder out of the current user guess and replace any occurrence of the letter with the
+            //user's guessed letter
+            StringBuilder stringBuilder = new StringBuilder(userGuess);
+            for (int i = 0; i < wordToGuess.length(); i++) {
+                if(wordToGuess.charAt(i) == charAt){  // if the char exist replace it at that index
+                    stringBuilder.setCharAt(i,charAt);
+                }
+            }
+
+            // if the user guess and stringBuilder are not equal that means the user had a correct guess
+            // update the  userGuess string and mark the current guess as correct
+            if(!userGuess.equals(stringBuilder.toString())){
+                userGuess = stringBuilder.toString();
+                correctGuess = true;
+            }
+        }
+
+        //output if the guess is correct or not
+        guessOutput(correctGuess);
+
+        //return the userGuess string (potentially updated)
+        return userGuess;
+    }
+
+    //outputs whether the guess was correct
+    private void guessOutput(boolean correctGuess) {
+        System.out.println("Your guess was " + correctGuess);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! Welcome to the game of Hangman!");
@@ -7,11 +9,28 @@ public class Main {
         String wordToGuess = dictionary.getWordToGuess();
         System.out.println(wordToGuess);
 
-        playGame(wordToGuess);
+        //Create the class that handles game logic, and start a new game
+        HangmanGame hangmanGame = new HangmanGame(wordToGuess);
+        hangmanGame.startGame();
+
+        //Create a scanner to read items from the console;
+        Scanner scanner = new Scanner(System.in);
+
+        //Get a string that represents the correct guesses that the user has made so far
+        //here it will be a string filled with "_"
+        String userGuess = hangmanGame.getUserGuess();
+
+        //Begin reading the input and checking to see if it exists in the word they are guessing
+        while (!userGuess.equals(wordToGuess)){
+            String guess = scanner.next();
+            //if more than one letter is given, take the first letter
+            userGuess = hangmanGame.checkUserGuess(guess.charAt(0));
+            System.out.println(hangmanGame.getUserGuess());
+
+        }
+
+
 
     }
 
-    private static void playGame(String wordToGuess) {
-        System.out.println("The word to guess has " + wordToGuess.length() + " letters!");
-    }
 }
