49c2698a18c03cb75528ef213e84962704a6f275
Brady Sillito <sillito121@gmail.com>
2022-10-29 11:13:46 -0600

    Added the fill dictionary fyunction

---
+++ src/Dictionary.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Dictionary {
+    private List<String> words;
+    private String currentWord;
+    private final String FILE_PATH = "/Users/bradysillito/Desktop/Misc Files/Hangman/src/dictionary.txt";
+
+    //Dictionary initializes class variables and fills itself with words from a textFile;
+
+    public Dictionary() {
+        words = new ArrayList<>();
+
+        try {
+            fillDictionary();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Something went wrong when filling the dictionary");
+        }
+    }
+
+    private void fillDictionary() throws FileNotFoundException {
+        File file = new File(FILE_PATH);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNext()){
+            words.add(scanner.next());
+        }
+
+        scanner.close();
+    }
+
+    public String getWordToGuess(){
+        this.currentWord = words.get(0);
+        return currentWord;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! Welcome to the game of Hangman!");
+
+
+        Dictionary dictionary = new Dictionary();
+        String wordToGuess = dictionary.getWordToGuess();
+        System.out.println(wordToGuess);
+
+
     }
 }
